34d10edf9795a8ade108fba312f92e5ac3252acd
Aaron Marburg <aaron@apl.washington.edu>
2018-06-12 11:30:19 -0700

    Unit test actually does something.

--- test/unit/test_test.cpp
+++ test/unit/test_test.cpp
@@ -2,8 +2,13 @@
 #include <iostream>
 #include <gtest/gtest.h>
 
+#include "libg3logger/g3logger.h"
+
 using namespace std;
 
 TEST(test_case_name, test_name) {
   ASSERT_TRUE( true );
+
+  libg3log::G3Logger logWorker( "test_name" );
+  logWorker.logBanner();
 }
